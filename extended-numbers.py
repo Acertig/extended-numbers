709ff349a8e618873793a64fa2653ed302f889be
syster <67067768+Acertig@users.noreply.github.com>
2021-06-16 22:34:48 -0500

    Update extended-numbers.py

--- extended-numbers.py
+++ extended-numbers.py
@@ -1,30 +1,34 @@
-class AutoAttributeSetter:
-    def __setattr__(self, attr, value): 
-        if not (getattr(self, attr, value) and callable(value) and attr.startswith("__")): 
-            super().__setattr__(attr, value)
 
-class ExtendedNumber(AutoAttributeSetter): 
+class ExtendedNumber(): 
 
     CONVERSIONS = {"k" : 3, "M" : 6, "B" : 9, "T" : 12}
 
     def __init__(self, number, **kwargs): 
         self.__number = number
-        self.__precision = kwargs.get("precision", 1)
-        self.__instanceable = kwargs.get("instanceable", False)
-        self.__exnumber = None
-        self.__update_exnumber()
+        self.precision = kwargs.get("precision", 1)
+        self.exnumber = None
 
-    def __add__(self, other : int): 
-        if self.__instanceable:
-            return ExtendedNumber(self.__number + other, precision = self.__precision, instanceable = self.__instanceable) 
-        self.__number += other
-        self.__update_exnumber()
-        return self
-    
+        self.update_exnumber()
+
+    def __add__(self, other): 
+        if isinstance(other, ExtendedNumber):
+            return ExtendedNumber(self.number + other.number, precision = self.precision)
+        return ExtendedNumber(self.number + other, precision = self.precision)
+
+    def __sub__(self, other): 
+        if isinstance(other, ExtendedNumber): 
+            return ExtendedNumber(self.number - other, percision = self.precision)
+        return ExtendedNumber(self.number - other.number, precision = self.precision)
+        
     __radd__ = __add__
+    __rsub__ = __sub__
 
-    def __str__(self : int) -> str: 
-        return self.__exnumber
+    def __repr__(self): 
+        return self.exnumber
+
+    @property
+    def number(self): 
+        return self.__number
 
     @staticmethod
     def convert(number : str, precision : int) -> str:
@@ -40,15 +44,15 @@ class ExtendedNumber(AutoAttributeSetter):
             before, after = (number[0:diff], number[diff:diff + precision])
             return before + "." + after + props[2]
         else: 
-            to_return = str(round(0.001 * int(number), 4)) + "k"
+            to_return = str(round(0.001 * int(number), 4)) + list(__class__.CONVERSIONS.keys())[0]
             to_return = to_return if not negative else "-" + to_return
             return to_return
 
-    def __update_exnumber(self) -> None:
-        self.__exnumber = self.convert(str(self.__number), self.__precision)
+    def update_exnumber(self) -> None:
+        self.exnumber = self.convert(str(self.number), self.precision)
 
     def custom_conversions(self, custom_conversions : dict) -> None:
         self.__class__.CONVERSIONS = custom_conversions
-        self.__update_exnumber()
+        self.update_exnumber()
 
-# Example use : number = ExtendedNumber(450, precision = 3, instanceable = True)
+# Example use : number = ExtendedNumber(450, precision = 3)
